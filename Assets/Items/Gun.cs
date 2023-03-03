4cb8e093738f5783cb8807953b4173f00f45633a
Josh Pattman <Josh.pattman@gmail.com>
2023-03-03 23:02:21 +0000

    Animations

--- Assets/Items/Gun.cs
+++ Assets/Items/Gun.cs
@@ -7,9 +7,18 @@ public class Gun : MonoBehaviour
     public Transform muzzle;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10f;
+
+    public bool isFullAuto = false;
+
+    Animator animator;
+
+    public float bulletTimeout = 0.25f;
+
+    public float spread = 0.1f;
+
     void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
     
     void Start()
@@ -19,10 +28,13 @@ public class Gun : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        var canShoot = animator.GetCurrentAnimatorStateInfo(0).IsName("Idle");
+        if (canShoot && ((Input.GetMouseButtonDown(0) && !isFullAuto) || (Input.GetMouseButton(0) && isFullAuto)))
         {
+            var spreadVel = Vector3.ProjectOnPlane(Random.insideUnitSphere, muzzle.forward).normalized * spread;
             var bullet = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed+spreadVel;
+            animator.Play("Shoot");
         }
     }
 }
