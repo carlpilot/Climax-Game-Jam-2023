2d55eb0776560d5faaaa4486a95a098c18b13603
carlpilot <carlbert101@gmail.com>
2023-03-03 19:35:46 +0000

    Rearrange files

--- Assets/Scripts/MazeGeneration/MazeMaker.cs
+++ Assets/Scripts/MazeGeneration/MazeMaker.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateMaze : MonoBehaviour {
+public class MazeMaker : MonoBehaviour {
 
     public float chunkSize { get => passagewayWidth * chunkNumCells; }
 
