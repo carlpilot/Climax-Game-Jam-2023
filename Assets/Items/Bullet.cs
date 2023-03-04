fe81506f6d2f2a3c4b7d8ca31d031cdacb1f0a94
Josh Pattman <Josh.pattman@gmail.com>
2023-03-04 10:30:51 +0000

    Fixed bullets and added sword

--- Assets/Items/Bullet.cs
+++ Assets/Items/Bullet.cs
@@ -7,17 +7,25 @@ public class Bullet : MonoBehaviour
     public GameObject hitParticlesPrefab;
     public float directDamage = 20f;
     public float knockbackForce = 10f;
-    void OnTriggerEnter(Collider col)
+
+    void Update()
     {
-        var p = Instantiate(hitParticlesPrefab, transform.position, Quaternion.identity);
-        p.SetActive(true);
-        gameObject.SetActive(false);
-        var enemy = col.gameObject.GetComponent<Enemy>();
-        if (enemy)
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 0.1f);
+        foreach (var col in hitColliders)
         {
-            enemy.TakeDamage(directDamage);
-            enemy.Knockback(transform.forward, knockbackForce);
+            var p = Instantiate(hitParticlesPrefab, transform.position, Quaternion.identity);
+            p.SetActive(true);
+            gameObject.SetActive(false);
+            var enemy = col.gameObject.GetComponent<Enemy>();
+            if (enemy)
+            {
+                enemy.TakeDamage(directDamage);
+                enemy.Knockback(transform.forward, knockbackForce);
+            }
         }
-        Destroy(gameObject);
+        if (hitColliders.Length > 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
